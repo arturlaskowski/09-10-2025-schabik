fa1feebf1c45fb9ec7b33c1a461eabb9841b605b
Artur Laskowski <artur.laskowski@comarch.com>
2024-09-19 21:48:34 +0200

    Different approach to implementing CustomerRepository

--- src/main/java/pl/schabik/repository/SqlCustomerRepository.java
+++ src/main/java/pl/schabik/repository/SqlCustomerRepository.java
@@ -1,38 +1,9 @@
 package pl.schabik.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import pl.schabik.model.Customer;
 
-import java.util.Optional;
 import java.util.UUID;
 
-@Repository
-public class SqlCustomerRepository implements CustomerRepository {
-
-    private final JpaCustomerRepository jpaCustomerRepository;
-
-    public SqlCustomerRepository(JpaCustomerRepository jpaCustomerRepository) {
-        this.jpaCustomerRepository = jpaCustomerRepository;
-    }
-
-    @Override
-    public Customer save(Customer customer) {
-        return jpaCustomerRepository.save(customer);
-    }
-
-    @Override
-    public Optional<Customer> findById(UUID id) {
-        return jpaCustomerRepository.findById(id);
-    }
-
-    @Override
-    public boolean existsByEmail(String email) {
-        return jpaCustomerRepository.existsByEmail(email);
-    }
-}
-
-@Repository
-interface JpaCustomerRepository extends JpaRepository<Customer, UUID> {
-    boolean existsByEmail(String email);
+public interface SqlCustomerRepository extends CustomerRepository, JpaRepository<Customer, UUID> {
 }
