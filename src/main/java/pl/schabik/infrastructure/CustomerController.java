db7ef8fa8981b90dbd3647efad83a7db686dfea7
Artur Laskowski <artur.laskowski@comarch.com>
2025-10-04 14:44:24 +0200

    update Spring Boot version to 3.5.3

--- src/main/java/pl/schabik/infrastructure/CustomerController.java
+++ src/main/java/pl/schabik/infrastructure/CustomerController.java
@@ -3,9 +3,11 @@ package pl.schabik.infrastructure;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.schabik.usecase.createcustomer.CreateCustomerService;
 import pl.schabik.usecase.getcustomer.GetCustomerService;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -28,9 +30,16 @@ public class CustomerController {
     }
 
     @PostMapping
-    public UUID addCustomer(@RequestBody @Valid CreateCustomerRequest createCustomerRequest) {
+    public ResponseEntity<Void> addCustomer(@RequestBody @Valid CreateCustomerRequest createCustomerRequest) {
         var createCustomerDto = CustomerApiMapper.mapToCreateCustomerDto(createCustomerRequest);
-        return createCustomerService.addCustomer(createCustomerDto);
+        var customerId = createCustomerService.addCustomer(createCustomerDto);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(customerId)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
 
--- src/main/java/pl/schabik/infrastructure/OrderController.java
+++ src/main/java/pl/schabik/infrastructure/OrderController.java
@@ -2,13 +2,15 @@ package pl.schabik.infrastructure;
 
 
 import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.schabik.usecase.createorder.CreateOrderService;
 
-import java.util.UUID;
+import java.net.URI;
 
 @RestController
 @RequestMapping("/orders")
@@ -21,8 +23,15 @@ public class OrderController {
     }
 
     @PostMapping
-    public UUID createOrder(@RequestBody @Valid CreateOrderRequest createOrderRequest) {
+    public ResponseEntity<Void> createOrder(@RequestBody @Valid CreateOrderRequest createOrderRequest) {
         var createOrderDto = OrderApiMapper.mapToDto(createOrderRequest);
-        return createOrderService.createOrder(createOrderDto).id();
+        var orderId = createOrderService.createOrder(createOrderDto);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(orderId.id())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
