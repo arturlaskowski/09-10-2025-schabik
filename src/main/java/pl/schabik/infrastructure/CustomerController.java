9ea59b8738fe09aa3fbc247fd80e5594110ab931
Artur Laskowski <artur.laskowski@comarch.com>
2025-10-04 14:44:24 +0200

    update Spring Boot version to 3.5.3

--- src/main/java/pl/schabik/infrastructure/CustomerController.java
+++ src/main/java/pl/schabik/infrastructure/CustomerController.java
@@ -3,8 +3,12 @@ package pl.schabik.infrastructure;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import pl.schabik.application.CustomerService;
+import pl.schabik.application.dto.CreateCustomerDto;
+import pl.schabik.application.dto.CustomerDto;
 
+import java.net.URI;
 import java.util.UUID;
 
 @RestController
@@ -18,16 +22,21 @@ public class CustomerController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CustomerResponse> getCustomer(@PathVariable UUID id) {
-        var customer = customerService.getCustomer(id);
-        var customerResponse = CustomerApiMapper.mapToCustomerResponse(customer);
-        return ResponseEntity.ok(customerResponse);
+    public ResponseEntity<CustomerDto> getCustomer(@PathVariable UUID id) {
+        var customerDto = customerService.getCustomer(id);
+        return ResponseEntity.ok(customerDto);
     }
 
     @PostMapping
-    public UUID addCustomer(@RequestBody @Valid CreateCustomerRequest createCustomerRequest) {
-        var createCustomerDto = CustomerApiMapper.mapToCreateCustomerDto(createCustomerRequest);
-        return customerService.addCustomer(createCustomerDto);
+    public ResponseEntity<Void> addCustomer(@RequestBody @Valid CreateCustomerDto createCustomerDto) {
+        var customerId = customerService.addCustomer(createCustomerDto);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(customerId)
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
 
