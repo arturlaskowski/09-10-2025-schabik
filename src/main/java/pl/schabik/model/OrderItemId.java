759898d06b39749c8d1b0645905b662b8f2cfc23
Artur Laskowski <artur.laskowski@comarch.com>
2025-10-04 15:21:22 +0200

    Refactor OrderItemId to use record type; update Spring Boot version to 3.5.3

--- src/main/java/pl/schabik/model/OrderItemId.java
+++ src/main/java/pl/schabik/model/OrderItemId.java
@@ -1,22 +1,7 @@
 package pl.schabik.model;
 
-import java.util.Objects;
-
-public class OrderItemId {
-
-    private Integer id;
-    private Order order;
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        OrderItemId that = (OrderItemId) o;
-        return Objects.equals(id, that.id) && Objects.equals(order, that.order);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, order);
-    }
+public record OrderItemId(
+        Integer id,
+        Order order) {
 }
 
