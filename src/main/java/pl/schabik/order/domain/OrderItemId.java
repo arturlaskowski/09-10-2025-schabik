603a618b3d6611b2a39f3725a0e9a791edc3db9d
Artur Laskowski <artur.laskowski@comarch.com>
2025-10-04 18:32:29 +0200

    update Spring Boot version to 3.5.3

--- src/main/java/pl/schabik/order/domain/OrderItemId.java
+++ src/main/java/pl/schabik/order/domain/OrderItemId.java
@@ -1,26 +1,6 @@
 package pl.schabik.order.domain;
 
-import java.io.Serializable;
-
-
-class OrderItemId implements Serializable {
-
-    private Integer id;
-    private Order order;
-
-    public OrderItemId(Integer id, Order order) {
-        this.id = id;
-        this.order = order;
-    }
-
-    public OrderItemId() {
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public Order getOrder() {
-        return order;
-    }
+record OrderItemId(
+        Integer id,
+        Order order) {
 }
--- src/main/java/pl/schabik/order/domain/vo/OrderId.java
+++ src/main/java/pl/schabik/order/domain/vo/OrderId.java
@@ -5,23 +5,14 @@ import jakarta.persistence.Embeddable;
 import java.util.UUID;
 
 @Embeddable
-public class OrderId {
+public record OrderId(UUID orderId) {
 
     public static OrderId newOne() {
         return new OrderId(UUID.randomUUID());
     }
 
-    private UUID orderId;
-
-    public OrderId(UUID uuid) {
-        this.orderId = uuid;
-    }
-
-    protected OrderId() {
-    }
-
     public UUID id() {
         return orderId;
     }
-
 }
+
